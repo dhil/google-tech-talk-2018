6ac09324649cfe7dbe55565779bc407d46bb08ce
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2018-12-10 07:03:56 +0100

    Skeleton code for resumable exceptions program.

---
+++ code/resumable-exceptions.ml
@@ -0,0 +1,23 @@
+(* From exceptions to algebraic effects. *)
+
+exception Conversion_failure of string
+let int_of_string str =
+  match int_of_string str with
+  | success -> success
+  | exception Failure _ -> raise (Conversion_failure str)
+
+(* Attempts to read and convert strings from standard input to
+   integers.  *)
+let rec sum_up result =
+  match
+    let input = input_line stdin in
+    int_of_string input
+  with
+  | number -> sum_up (number + result)
+  | exception (Conversion_failure str) ->
+     failwith (Printf.sprintf "Conversion failure '%s'" str)
+  | exception End_of_file -> result
+
+let _ =
+  let sum = sum_up 0 in
+  Printf.printf "Sum is %d\n" sum
